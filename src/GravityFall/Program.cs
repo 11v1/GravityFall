66215971768550c7be06da945847b4f0a34f71b5
Grigoriy Vasilchenko <grigoriy.vasilchenko@gmail.com>
2021-08-23 17:58:59 +0700

    Add project files.

---
+++ src/GravityFall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GravityFall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
