067259b9882620610e60597b3eb0bd03bbde3dc2
Grigoriy Vasilchenko <grigoriy.vasilchenko@gmail.com>
2021-08-24 20:58:29 +0700

    Added first game test

--- test/GravityFallTests/GamePlayerTests.cs
+++ test/GravityFallTests/GamePlayerTests.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Aura.GravityFall.Actions;
+using Ninject;
+using Ninject.Extensions.Factory;
 
 namespace Aura.GravityFall.Tests
 {
@@ -62,10 +64,36 @@ namespace Aura.GravityFall.Tests
             }));
         }
 
-        //[TestMethod()]
-        //public void GetShortestSolutionTest()
-        //{
-        //    Assert.Fail();
-        //}
+        [TestMethod()]
+        public void GameTest1()
+        {
+            // arrange
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IGameboardFactory>().ToFactory();
+            kernel.Bind<IGameboard>().To<Gameboard>();
+            kernel.Bind<IGameboardSnapshot>().To<GameboardSnapshot>();
+
+
+            Gameboard gameboard = (Gameboard)kernel.Get<IGameboardFactory>().CreateGameboard(10, 10, new List<GameboardObject>()
+            {
+                new GameboardObject() { Number = 1, X = 1, Y = 1 }
+            }, new List<GameboardObject>()
+            {
+                new GameboardObject() { Number = 1, X = 1, Y = 2 }
+            });
+
+            // act
+            GamePlayer gamePlayer = new(gameboard);
+            var solution = gamePlayer.GetShortestSolution(new List<IAction>()
+            {
+                new GravityBottomAction(),
+                new GravityTopAction(),
+                new GravityLeftAction(),
+                new GravityRightAction(),
+            });
+
+            // assert
+            Assert.IsNotNull(solution);
+        }
     }
 }
